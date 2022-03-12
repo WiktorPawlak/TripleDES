3bd458338aded930dded6613e29efc0d998f778b
Antoni Jończyk <236551@edu.p.lodz.pl>
2022-03-12 22:41:53 +0100

    kitty cat

--- des/des.fsx
+++ des/des.fsx
@@ -14,12 +14,38 @@ module conv =
             let padded = Array.append last padding
             Array.set chunks ((Array.length chunks) - 1)  padded
         chunks
-        
-        
 
-    let toBlocks input  =
-        input 
+    let unpad (chunks:array<array<byte>>) =
+        let lastChunk = Array.last chunks
+        let lastByte = Array.last lastChunk
+        let padding = (int lastByte)
+        let contentLength = 8 - padding
+        if padding <= 7 then // maximum padding
+            let count = [contentLength + 1  .. chunks.Length]
+                         |> List.map (Array.get lastChunk)
+                         |> List.forall (fun x -> x = lastByte)
+            if count then
+                let truncated = Array.take contentLength lastChunk
+                Array.set chunks ((Array.length chunks) - 1)  truncated
+        chunks
+                
+                
+                
+
+
+    let toBlocks bytes  =
+        bytes 
         |> Array.chunkBySize 8 // 64 bits
         |> pad
-        |> Array.map BitArray
+        |> Array.map BitArray // BitVector byłby szybszy, ale ma za małą pojemność
 
+    let BitArrayToBytes (bits:BitArray) =
+        let ans = Array.replicate 8 0uy
+        bits.CopyTo(ans, 0)
+        ans
+    
+    let toBytes blocks =
+        blocks
+        |> Array.map BitArrayToBytes
+        |> unpad
+        |> Array.concat
