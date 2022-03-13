d25edabb68f9ebbf899b3754c5175aff59670c2c
Antoni Jończyk <236551@edu.p.lodz.pl>
2022-03-13 18:12:34 +0100

    ...

--- des/des.fsx
+++ des/des.fsx
@@ -77,8 +77,10 @@ module crypt =
         let initial= [ 57; 49; 41; 33; 25; 17; 9; 1; 59; 51; 43; 35; 27; 19; 11; 3; 61; 53; 45; 37; 29; 21; 13; 5; 63; 55; 47; 39; 31; 23; 15; 7; 56; 48; 40; 32; 24; 16; 8; 0; 58; 50; 42; 34; 26; 18; 10; 2; 60; 52; 44; 36; 28; 20; 12; 4; 62; 54; 46; 38; 30; 22; 14; 6]
         let reverse= [39; 7; 47; 15; 55; 23; 63; 31; 38; 6; 46; 14; 54; 22; 62; 30; 37; 5; 45; 13; 53; 21; 61; 29; 36; 4; 44; 12; 52; 20; 60; 28; 35; 3; 43; 11; 51; 19; 59; 27; 34; 2; 42; 10; 50; 18; 58; 26; 33; 1; 41; 9; 49; 17; 57; 25; 32; 0; 40; 8; 48; 16; 56; 24 ]
         let E= [31; 0; 1; 2; 3; 4; 3; 4; 5; 6; 7; 8; 7; 8; 9; 10; 11; 12; 11; 12; 13; 14; 15; 16; 15; 16; 17; 18; 19; 20; 19; 20; 21; 22; 23; 24; 23; 24; 25; 26; 27; 28; 27; 28; 29; 30; 31; 0; ]
+        let P= [15; 6; 19; 20; 28; 11; 27; 16; 0; 14; 22; 25; 4; 17; 30; 9; 1; 7; 23; 13; 31; 26; 2; 8; 18; 12; 29; 5; 21; 10; 3; 24]
 
-
+            
+        // można by tą tablicę zoptymalizować tak, aby uzyskanie adresu było kwestią konkatenacji numeru S (3 bity) i tych 6 bitów adresu (1 tabela, 512 pozycji)
         let Sraw = [[14; 4; 13; 1; 2; 15; 11; 8; 3; 10; 6; 12; 5; 9; 0; 7; 0; 15; 7; 4; 14; 2; 13; 1; 10; 6; 12; 11; 9; 5; 3; 8; 4; 1; 14; 8; 13; 6; 2; 11; 15; 12; 9; 7; 3; 10; 5; 0; 15; 12; 8; 2; 4; 9; 1; 7; 5; 11; 3; 14; 10; 0; 6; 13; ];
                     [ 15; 1; 8; 14; 6; 11; 3; 4; 9; 7; 2; 13; 12; 0; 5; 10; 3; 13; 4; 7; 15; 2; 8; 14; 12; 0; 1; 10; 6; 9; 11; 5; 0; 14; 7; 11; 10; 4; 13; 1; 5; 8; 12; 6; 9; 3; 2; 15; 13; 8; 10; 1; 3; 15; 4; 2; 11; 6; 7; 12; 0; 5; 14; 9];
                     [ 10; 0; 9; 14; 6; 3; 15; 5; 1; 13; 12; 7; 11; 4; 2; 8; 13; 7; 0; 9; 3; 4; 6; 10; 2; 8; 5; 14; 12; 11; 15; 1; 13; 6; 4; 9; 8; 15; 3; 0; 11; 1; 2; 12; 5; 10; 14; 7; 1; 10; 13; 0; 6; 9; 8; 7; 4; 15; 14; 3; 11; 5; 2; 12 ];
@@ -107,6 +109,9 @@ module crypt =
 
     let keySchedule (key:BitArray) n  =
         key
+
+    let S (n, addr) =
+        tables.Sraw[n][addr]
     
     let makeSAddress bits =
         let unpacked = bits |> Array.map (fun b -> if b then 1 else 0)
@@ -114,7 +119,12 @@ module crypt =
         let j = unpacked[1] * 8 + unpacked[2] * 4 + unpacked[3] * 2 + unpacked[4]
         i * 16 + j
         
-
+    
+    let ConcatenateFourBitPieces (arr:array<int>) =
+        let mutable acc = 0
+        for i in arr do
+            acc <- ((acc <<< 4) ||| i)        
+        BitArray (Array.singleton acc)
         
     let toSixBitAddresses (bits:BitArray) =
         let arr = (Array.replicate 48 false)
@@ -122,6 +132,9 @@ module crypt =
         arr
         |> Array.chunkBySize 6
         |> Array.map makeSAddress
+        |> Array.indexed
+        |> Array.map S
+        |> ConcatenateFourBitPieces
 
     
     let cipher (keyPart:BitArray) (bits:BitArray) = // the $f$ function
