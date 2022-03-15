97c8e367f8268d756a69613b87c4f7c52dbbd71e
Antoni Jończyk <236551@edu.p.lodz.pl>
2022-03-15 11:30:41 +0100

    ...

--- des/des.fsx
+++ des/des.fsx
@@ -206,24 +206,3 @@ module crypt =
         |> cryptBlock keyList
         |> permutations.reverse
         
-
-module debug =
-
-    let bools2hex bin =
-        bin
-        |> Array.map (fun i -> if i then 1 else 0)
-        |> Array.reduce (fun a b -> (a * 2) + b)
-        |> sprintf "%x"
-
-
-    let hex2bools str =
-        let num =
-            System.Int32.Parse(str, System.Globalization.NumberStyles.AllowHexSpecifier)
-        [| 3..-1..0 |]
-        |> Array.map (fun i -> (num >>> i) % 2 = 1)
-
-    
-    let toStr (bits:BitArray) =
-        let inter = (Array.replicate bits.Length false)
-        bits.CopyTo(inter, 0)
-        inter |> Array.chunkBySize 4 |> Array.map bools2hex
