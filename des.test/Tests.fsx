b9082c6c4fc17f7496673f98f1f146d9e13e70c2
Antoni Jończyk <236551@edu.p.lodz.pl>
2022-03-15 21:18:16 +0100

    tdea

--- des.test/Tests.fsx
+++ des.test/Tests.fsx
@@ -2,6 +2,7 @@ module Tests
 
 #if INTERACTIVE
 #load "../des/des.fsx"
+#load "../des/tdea.fsx"
 #load "../des/debug.fsx"
 #r "nuget:xunit"
 #endif
@@ -19,3 +20,20 @@ let ``des enc/dec test`` () =
     let decrypted = crypt.decryptBlock key encrypted
     Assert.Equal(expectedEncrypted, toStr encrypted)
     Assert.Equal(toStr text, toStr decrypted)
+
+[<Fact>]
+let ``tdea działa`` () =
+    let key1 = toBA "abb098376cd1ca2d"
+    let key2 = toBA "a1b27ab60c89dd3c"
+    let key3 = toBA "b0ba17a6298cd3cd"
+    let keys = (key1, key2, key3)
+    let plaintext = "c0b78d05af3a829c"
+
+    let after =
+        plaintext
+        |> toBA
+        |> tdea.encryptBlock keys
+        |> tdea.decryptBlock keys
+        |> toStr
+
+    Assert.Equal(plaintext, after)
