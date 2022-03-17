24e82359829b410fecc5b373ef8aadfa807c36de
Antoni Jończyk <236551@edu.p.lodz.pl>
2022-03-17 18:09:59 +0100

    CBC

--- des/tdea.fsx
+++ des/tdea.fsx
@@ -5,6 +5,8 @@ module tdea
 #load "conv.fs"
 #endif
 
+open System.Collections
+
 let encryptBlock (k1, k2, k3) block =
     block
     |> des.encrypt k1
@@ -17,3 +19,27 @@ let decryptBlock (k1, k2, k3) block =
     |> des.decrypt k3
     |> des.encrypt k2
     |> des.decrypt k1
+
+let encrypt iv keys data =
+
+    let CBC (previous: BitArray) plaintext =
+        let xored = (BitArray previous).Xor plaintext
+        encryptBlock keys xored
+
+    data
+    |> conv.toBlocks
+    |> Array.scan CBC iv
+    |> conv.toBytes
+
+let decrypt iv keys data =
+
+    let CBC ((previous: BitArray), _) ciphertext =
+        let decrypted = decryptBlock keys ciphertext
+        let plaintext = (BitArray previous).Xor decrypted
+        (ciphertext, plaintext)
+
+    data
+    |> conv.toBlocks
+    |> Array.scan CBC (iv, iv)
+    |> Array.map snd
+    |> conv.toBytes
