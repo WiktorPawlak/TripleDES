8ac06780ae7548748290aeb2456ef2eb2eeaf7ba
236621 Wiktor Pawlak <236621@edu.p.lodz.pl>
2022-03-18 20:46:19 +0100

    First view with preliminary bindings

---
+++ desViewModel/DesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace desViewModel
+{
+    public class DesController : INotifyPropertyChanged
+    {
+        public string Key1 { get; set; }
+        public string? Key2 { get; set; }
+        public string? Key3 { get; set; }
+        public string InitVector { get; set; }
+        public bool Option1 { get; set; } = true;
+        public bool Option2 { get; set; }
+        public bool Option3 { get; set; }
+        public string? EncryptFileName { get; set; } = "filename...";
+        public string? DecryptFileName { get; set; } = "filename...";
+        public string? EncryptText { get; set; } = "text...";
+        public string? DecryptText { get; set; } = "text...";
+        public string? Result { get; set; } = "Result...";
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+
+    }
+}
