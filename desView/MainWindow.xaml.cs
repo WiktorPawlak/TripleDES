7454d1d6ab65bafd70eef96ac7e29608a16e5c9a
236621 Wiktor Pawlak <236621@edu.p.lodz.pl>
2022-03-18 20:46:19 +0100

    First view with preliminary bindings

---
+++ desView/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using desViewModel;
+using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DesView
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new DesController();
+        }
+    }
+}
